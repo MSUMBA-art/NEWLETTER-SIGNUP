2a6b77ba2f6b2dd112c6e8288a915f4574007e90
ALEXANDER <msumba83@gmail.com>
2021-11-16 06:13:39 +0200

    installed packages

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
-
-const https = require("https");
 const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
 
-app.get("/", function(req, res) {
-    res.send("Hello World")
-});
+// app.get("/", function(req, res) {
+//     res.send("HALLO, HOW ARE YOU guys")
+// });
 
-app.listen()
+app.listen(3000, function(req, res) {
+    console.log("server is running on port 3000")
+});
